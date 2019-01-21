641240bfccfd22a6190cc378ae7c8a4ba8a9b6d1
valentinmoulard <moulardvalentin@gmail.com>
2019-01-21 15:59:08 +0100

    Color swap et calibrage

---
+++ Assets/Scripts/MovementYellowShip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+public class MovementYellowShip : MonoBehaviour
+{
+    void Update()
+    {
+        float x = ColorDetection.yellowTuple.Item1;
+        float y = ColorDetection.yellowTuple.Item2;
+        
+        //Debug.Log("yellow : " + x + " - " + y);
+        //resolution de la webcam : 620 x 460
+        //taille de la camera : 40 x 20
+        //ce sont les calcul pour cadrer les vaisseau dans la camera
+        float posX = (x / 620.0f) * 40.0f;
+        float posY = (((y - 460) / 460.0f) * 20.0f) * -1.0f;
+
+        gameObject.transform.position = new Vector3(posX, posY);
+
+    }
+}
