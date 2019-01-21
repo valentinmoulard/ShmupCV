88bb62f465472361c349435f209fe748cd8d15fd
Piteul <axel.tetart@gmail.com>
2019-01-21 11:14:04 +0100

    Add new assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        this.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
