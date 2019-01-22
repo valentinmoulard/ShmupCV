7014ca7eb3ffcd731609ecfce655593f82ce4e8b
Piteul <axel.tetart@gmail.com>
2019-01-22 14:55:01 +0100

    Wave Gestion :ocean:

--- Assets/Scripts/DestroyArea.cs
+++ Assets/Scripts/DestroyArea.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyArea : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
     }
---
+++ Assets/Scripts/WaveGestion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGestion : MonoBehaviour
+{
+
+    public GameObject[] waves;
+    private int currentWave;
+
+    IEnumerator Start()
+    {
+        if (waves.Length == 0)
+        {
+            yield break;
+        }
+
+        while (true)
+        {
+            //Make wave
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+
+            //Wave make Emitter to Child elements.
+            wave.transform.parent = this.transform;
+
+            // Wait for all Enemy child elements is deleted .
+            while (wave.transform.childCount != 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            Destroy(wave);
+
+            // When Wave stored have executed all, currentWave makes 0.（Initialize -> loop）.
+            if (waves.Length <= ++currentWave)
+            {
+                yield break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
