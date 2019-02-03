468a341976722e88aba0e286f52c8e8f1b604db5
Veen <moulardvalentin@gmail.com>
2019-02-03 12:20:42 +0100

    background music

---
+++ Assets/Scripts/MusicPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlay : MonoBehaviour
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
