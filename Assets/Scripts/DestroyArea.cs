7036439ce3bdf9ddfe9dbf9e99dd301fa3dca2c6
Piteul <axel.tetart@gmail.com>
2019-02-04 13:39:07 +0100

    Finish Level 1 :one:

--- Assets/Scripts/DestroyArea.cs
+++ Assets/Scripts/DestroyArea.cs
@@ -6,7 +6,7 @@ public class DestroyArea : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
-       if(!collision.CompareTag("Player") || !collision.CompareTag("Boss")){
+       if(!collision.CompareTag("Player") && !collision.CompareTag("Boss")){
             Destroy(collision.gameObject);
         }
     }
